100fdd7586e5f8fd8ce8809f2bf09d96dbe80d89
codeAWgit <36483111+codeAWgit@users.noreply.github.com>
2018-02-14 12:58:45 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let nums = [100,25,12,72]
+
+let num = 23
+
+function setup(){
+	createCanvas(400,400)
+}
+
+function draw(){
+	background(0)
+	ellipse(100,200,num,num)
+	rect(50,50,20,20)
+
+}
+
+console.log("Hi ya.")
